113e6456bfb2f0166796abe85c5e3a19d64981a5
Diego-coder2525 <forgam2525@outlook.es>
2023-05-06 14:10:10 -0500

    First commit

---
+++ src/pages/articles.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const articles = () => {
+  return (
+    <div>articles - Developing</div>
+  )
+}
+
+export default articles
---
+++ src/pages/projects.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const projects = () => {
+  return (
+    <div>projects - Developing</div>
+  )
+}
+
+export default projects
